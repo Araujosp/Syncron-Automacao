1a789871c7b21ecee8b414b53248e68fbfab8e9a
GABRIEL ARAUJO JANUARIO DE FREITAS <25254286@educ123.sp.senai.br>
2026-05-18 08:45:38 -0300

    corrigindo footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,30 +1,17 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<footer>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <div class="footer-esquerda">
 
-    <title>Syncron Automação</title>
+        <img src="img/logo-footer.png" alt="">
 
-    <link rel="stylesheet" href="css/style.css">
+        <p>
+            A Syncron Automação fornece soluções
+            em automação e controle industrial.
+        </p>
 
-    <link rel="stylesheet"
-    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
-</head>
-
-<body>
-
-<header>
-
-    <div class="logo">
-        <img src="img/logo-header.png" alt="">
     </div>
 
-    <nav>
-        <a href="index.php">Home</a>
-        <a href="produtos.php">Produtos</a>
-        <a href="login.php">Login</a>
-    </nav>
+</footer>
 
-</header>
+</body>
+</html>
--- pages/index.php
+++ pages/index.php
@@ -1,9 +1,10 @@
-<?php include 'includes/header.php'; ?>
+<?php include '../includes/header.php'; ?>
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../assets/styles.css">
     <title>Document</title>
 </head>
 <body>
@@ -13,7 +14,7 @@
     
     </main>
     
-    <?php include 'includes/footer.php'; ?>
+    <?php include '../includes/footer.php'; ?>
     
 </body>
 </html>
