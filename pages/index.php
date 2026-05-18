f711a7f7e4aeffd38d7cad2b5e5e26ba254d6b93
NICOLE HANAI HATAKEYAMA <25254383@educ123.sp.senai.br>
2026-05-18 10:41:03 -0300

    index

--- pages/index.php
+++ pages/index.php
@@ -1,16 +1,33 @@
-<?php include '../includes/header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/styles.css">
+    <link rel="stylesheet" href="../assets/index.css">
     <title>Document</title>
 </head>
 <body>
-    <main>
+    <?php include '../includes/header.php'; ?>
+    <main class="main">
+     
     
-        <h1>Página inicial</h1>
+        <img src="../img/banner.png" class="banner">
+        <h1 class="produto">Produtos em destaque</h1>
+        <div class="centraliza">
+            <div class="cor">
+                <img src="../img/Clps.jpg">
+                <div class="info">
+                    <p>clipes elétricos</p>
+                    <h2 class="preco">R$ 25,00</h2>
+                </div>
+                <a href="#" class="botao">Adicionar ao carrinho</a>
+            </div>
+            <div class="cor"></div>
+            <div class="cor"></div>
+            <div class="cor"></div>
+        </div>
     
     </main>
     
