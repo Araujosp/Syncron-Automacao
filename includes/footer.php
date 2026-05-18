b6b29cc56a3e3f3afcc5d267a6a23d5cbb05a205
GABRIEL ARAUJO JANUARIO DE FREITAS <25254286@educ123.sp.senai.br>
2026-05-18 08:31:39 -0300

    criação do crud, header, footer e css

---
+++ includes/footer.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Syncron Automação</title>
+
+    <link rel="stylesheet" href="css/style.css">
+
+    <link rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+</head>
+
+<body>
+
+<header>
+
+    <div class="logo">
+        <img src="img/logo-header.png" alt="">
+    </div>
+
+    <nav>
+        <a href="index.php">Home</a>
+        <a href="produtos.php">Produtos</a>
+        <a href="login.php">Login</a>
+    </nav>
+
+</header>
---
+++ pages/index.php
@@ -0,0 +1,19 @@
+<?php include 'includes/header.php'; ?>
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <main>
+    
+        <h1>Página inicial</h1>
+    
+    </main>
+    
+    <?php include 'includes/footer.php'; ?>
+    
+</body>
+</html>
