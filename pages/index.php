8789dd130bcbecf4c0d784f798e33c6c1d5280ae
YuuY314 <rafaelyu0202@gmail.com>
2026-05-20 09:38:42 -0300

    Ajuste do cabeçalho e rodapé

--- pages/index.php
+++ pages/index.php
@@ -6,7 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../assets/styles.css">
     <link rel="stylesheet" href="../assets/index.css">
-    <title>Document</title>
+    <title>Home | Syncron</title>
+    <link rel="shortcut icon" href="../img/logo-favicon.png" type="image/png">
 </head>
 <body>
     <?php include '../includes/header.php'; ?>
