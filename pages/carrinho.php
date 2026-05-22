f8523b353da8c0f5b0af027d80e562b294f01a90
NICOLE HANAI HATAKEYAMA <25254383@educ123.sp.senai.br>
2026-05-22 10:48:21 -0300

    carrinho

---
+++ pages/carrinho.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="../assets/styles.css">
+    <link rel="stylesheet" href="../assets/carrinho.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>carrinho</title>
+</head>
+<body>
+    <?php include '../includes/header.php'; ?>
+    <main>
+        <div class="centraliza">
+            <section class="carrinho">
+                <h2>Meu carrinho:</h2>
+                <div class="caixa_pedidos">
+                    <div class="caixa">
+                        <img src="../img/clps.jpg" width="100x100">
+                        <div class="caixa_titulo">
+                            <p>CLPs eletricos</p>
+                            <div class="caixa_adicionar">
+                                <i class="fa-solid fa-minus"></i>
+                                <p>1</p>
+                                <i class="fa-solid fa-plus"></i>
+                            </div>
+                        </div>
+                        <div class="caixa_reais">
+                            <h1>R$ 25,00</h1>
+                            <i class="fa-solid fa-trash"></i>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="pagamento">
+                <a href="#" class="botao">continuar</a>
+            </section>
+        </div>
+    </main>
+    
+    
+</body>
+</html>
