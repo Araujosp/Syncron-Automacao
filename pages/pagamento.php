af8befed64441d1f22b067fca779b7a8029c9f0d
NICOLE HANAI HATAKEYAMA <25254383@educ123.sp.senai.br>
2026-06-09 09:23:26 -0300

    pagamento

--- pages/pagamento.php
+++ pages/pagamento.php
@@ -1,5 +1,25 @@
-<?php
 
-$teste = $_POST["teste"];
 
-echo $teste;
+
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once "../includes/meta-links.php"; ?>
+    <link rel="stylesheet" href="../assets/index.css">
+    <link rel="stylesheet" href="../assets/pagamento.css">
+    <title>Pagamento | Syncron</title>
+</head>
+<body>
+    <?php include '../includes/header.php'; ?>
+    <main>
+        <div class="pagamento">
+            <h1>Aguardando pagamento</h1>
+            <img src="../img/qr.png" alt="QR code">
+            <p>Este código expirará em</p>
+            <h1>2 horas</h1>
+        </div>
+        <aside></aside>
+    </main>
+</body>
